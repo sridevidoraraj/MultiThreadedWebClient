ffe549a6473c36eaea8538f5ef8a8832f114fb84
Sridevi <sridevi.elangovan@gcitsolutions.com>
2022-12-26 19:41:06 +0530

    MultiThreadedWebClient Program

---
+++ src/main/java/Client.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        File file = new File("D:\\Projects\\csweb\\index.html");
+
+        try {
+
+            Socket s = new Socket("localhost", 8080);
+            System.out.println("[CONNECTED]");
+
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String header = "GET / HTTP/1.0\r\n" + file
+                    + "Host:localhost\r\n\r\n";
+            byte[] byteHeader = header.getBytes();
+
+            DataOutputStream dos =new DataOutputStream(s.getOutputStream());
+
+            dos.write(byteHeader, 0, header.length());
+            dos.writeUTF(header);
+            String res = "";
+
+            byte[] buf = new byte[in.available()];
+            in.readFully(buf);
+            System.out.println("\t[READ PROCESS]");
+            System.out.println("\t\tbuff length->" + buf.length);
+            for (byte b : buf) {
+                res += (char) b;
+            }
+            System.out.println("\t[/READ PROCESS]");
+            System.out.println("[RES]");
+            System.out.println(res);
+            System.out.println("[CONN CLOSE]");
+
+            in.close();
+            dos.close();
+            s.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
