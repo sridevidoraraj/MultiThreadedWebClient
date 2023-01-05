e57e0e0e81c8aaa0c72d5e9432d926767f390ccb
Sridevi <sridevi.elangovan@gcitsolutions.com>
2023-01-05 12:57:01 +0530

    MultiThreadedWebClient Program - updated

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -5,43 +5,52 @@ public class Client {
 
     public static void main(String[] args) {
 
-        File file = new File("D:\\Projects\\csweb\\index.html");
+            File file = new File("D:\\Projects\\csweb\\index.html");
 
-        try {
+            try {
 
-            Socket s = new Socket("localhost", 8080);
-            System.out.println("[CONNECTED]");
+                Socket s = new Socket("localhost", 8080);
+                System.out.println("[CONNECTED]");
 
-            DataInputStream in = new DataInputStream(s.getInputStream());
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String header = "GET / HTTP/1.0\r\n" + file
-                    + "Host:localhost\r\n\r\n";
-            byte[] byteHeader = header.getBytes();
+                DataInputStream in = new DataInputStream(s.getInputStream());
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String header = "GET / HTTP/1.0\r\n" + file
+                        + "Host:localhost\r\n\r\n";
+                byte[] byteHeader = header.getBytes();
 
-            DataOutputStream dos =new DataOutputStream(s.getOutputStream());
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+                dos.write(byteHeader, 0, header.length());
+                dos.writeUTF(header);
+                String res = "";
 
-            dos.write(byteHeader, 0, header.length());
-            dos.writeUTF(header);
-            String res = "";
+                byte[] buf = new byte[in.available()];
+                in.readFully(buf);
+                System.out.println("\t[READ PROCESS]");
+                System.out.println("\t\tbuff length->" + buf.length);
+                for (byte b : buf) {
+                    res += (char) b;
+                }
+                System.out.println("\t[/READ PROCESS]");
+                System.out.println("[RES]");
+                System.out.println(res);
 
-            byte[] buf = new byte[in.available()];
-            in.readFully(buf);
-            System.out.println("\t[READ PROCESS]");
-            System.out.println("\t\tbuff length->" + buf.length);
-            for (byte b : buf) {
-                res += (char) b;
+                BufferedReader br1=new BufferedReader(new InputStreamReader(System.in));
+                String clientMessage="",serverMessage="";
+                while(!clientMessage.equals("bye")){
+                    clientMessage=br1.readLine();
+                    dos.writeUTF(clientMessage);
+                    dos.flush();
+                    serverMessage=in.readUTF();
+                    System.out.println(serverMessage);
+                }
+                System.out.println("[CONN CLOSE]");
+                in.close();
+                dos.close();
+                s.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            System.out.println("\t[/READ PROCESS]");
-            System.out.println("[RES]");
-            System.out.println(res);
-            System.out.println("[CONN CLOSE]");
-
-            in.close();
-            dos.close();
-            s.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        
     }
 
 }
